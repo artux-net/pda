dafd1c789c4c304fd696a329d017622f6a792b4c
Maxim <maksim.prygunov@gmail.com>
2023-01-17 22:01:26 +0400

    refactoring

--- core/src/net/artux/pda/map/engine/systems/DeadCheckerSystem.java
+++ core/src/net/artux/pda/map/engine/systems/DeadCheckerSystem.java
@@ -15,44 +15,33 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 
 import net.artux.pda.map.DataRepository;
-import net.artux.pda.map.di.scope.PerGameMap;
 import net.artux.pda.map.engine.components.HealthComponent;
 import net.artux.pda.map.engine.components.InteractiveComponent;
-import net.artux.pda.map.engine.components.Position;
+import net.artux.pda.map.engine.components.PositionComponent;
 import net.artux.pda.map.engine.components.SpriteComponent;
 import net.artux.pda.map.engine.components.StalkerComponent;
-import net.artux.pda.map.engine.components.TimeComponent;
-import net.artux.pda.map.engine.systems.player.PlayerSystem;
 import net.artux.pda.map.ui.UserInterface;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.Random;
 
-import javax.inject.Inject;
-
-@PerGameMap
 public class DeadCheckerSystem extends BaseSystem {
 
-    private Group gameZone;
-    private Label.LabelStyle labelStyle;
+    private final DataRepository dataRepository;
+    private final UserInterface ui;
+    private final Label.LabelStyle labelStyle;
+
+    private final ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+    private final ComponentMapper<HealthComponent> hm = ComponentMapper.getFor(HealthComponent.class);
+
+    private final AssetManager assetManager;
+    private final Random random = new Random();
     private boolean deadMessage;
-    private DataRepository dataRepository;
-    private UserInterface userInterface;
 
-    private ComponentMapper<Position> pm = ComponentMapper.getFor(Position.class);
-    private ComponentMapper<HealthComponent> hm = ComponentMapper.getFor(HealthComponent.class);
-
-    private AssetManager assetManager;
-    private Random random = new Random();
-
-    @Inject
     public DeadCheckerSystem(UserInterface userInterface, DataRepository dataRepository, AssetManager assetManager) {
-        super(Family.all(HealthComponent.class, Position.class).get());
-        this.gameZone = userInterface;
+        super(Family.all(HealthComponent.class, PositionComponent.class).get());
+        this.ui = userInterface;
         this.assetManager = assetManager;
-        this.userInterface = userInterface;
         this.dataRepository = dataRepository;
         labelStyle = userInterface.getLabelStyle();
         labelStyle.fontColor = Color.RED;
@@ -66,45 +55,39 @@ public class DeadCheckerSystem extends BaseSystem {
             Entity entity = getEntities().get(i);
 
             HealthComponent healthComponent = hm.get(entity);
-            Position position = pm.get(entity);
+            PositionComponent positionComponent = pm.get(entity);
 
             if (healthComponent.isDead()) {
                 if (random.nextInt(10) < 5) {
                     final Entity deadEntity = new Entity();
-                    deadEntity.add(new Position(position.getPosition()))
+                    deadEntity.add(new PositionComponent(positionComponent.getPosition()))
                             .add(new SpriteComponent(assetManager.get("gray.png", Texture.class), 4, 4));
 
-                    if (entity != getPlayer()) {
+                    if (entity != player) {
                         StalkerComponent stalkerComponent = entity.getComponent(StalkerComponent.class);
-                        deadEntity.add(new InteractiveComponent("Обыскать: " + stalkerComponent.getName(), 5, new InteractiveComponent.InteractListener() {
-                                    @Override
-                                    public void interact() {
-                                        switch (random.nextInt(4)) {
-                                            case 0:
-                                                dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("85:1")));
-                                                break;
-                                            case 1:
-                                                dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("83:2")));
-                                                break;
-                                            case 2:
-                                                dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("84:1")));
-                                                break;
-                                            case 3:
-                                                dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("85:2")));
-                                                break;
-                                            case 4:
-                                                dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("83:1")));
-                                                break;
-                                        }
+                        deadEntity.add(new InteractiveComponent("Обыскать: " + stalkerComponent.getName(), 5, userInterface -> {
+                            switch (random.nextInt(4)){
+                                case 0:
+                                    dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("85:1")));
+                                    break;
+                                case 1:
+                                    dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("83:2")));
+                                    break;
+                                case 2:
+                                    dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("84:1")));
+                                    break;
+                                case 3:
+                                    dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("85:2")));
+                                    break;
+                                case 4:
+                                    dataRepository.applyActions(Collections.singletonMap("add", Collections.singletonList("83:1")));
+                                    break;
+                            }
 
-                                        getEngine().removeEntity(deadEntity);
-                                    }
-                                }))
-                                .add(new TimeComponent(Instant.now().plus(1, ChronoUnit.MINUTES),
-                                        () -> getEngine().removeEntity(entity)))
-                                .add(stalkerComponent);
+                            getEngine().removeEntity(deadEntity);
+                        })).add(stalkerComponent);
                     } else {
-                        getEngine().removeEntity(getPlayer());
+                        getEngine().removeEntity(player);
                         getEngine().removeSystem(getEngine().getSystem(PlayerSystem.class));
                     }
 
@@ -113,9 +96,8 @@ public class DeadCheckerSystem extends BaseSystem {
                 getEngine().removeEntity(entity);
             }
         }
-        if (!isPlayerActive()) {
+        if (!getEngine().getEntities().contains(player, false)) {
             if (!deadMessage) {
-                System.out.println("Dead message from :" + this);
                 dataRepository.applyActions(Collections.singletonMap("xp", Collections.singletonList("-5")));
                 Group deadMessageGroup = new Group();
 
@@ -128,14 +110,14 @@ public class DeadCheckerSystem extends BaseSystem {
                 textButton.align(Align.center);
                 textButton.getLabel().setAlignment(Align.center);
                 deadMessageGroup.addActor(textButton);
-                userInterface.addListener(new ClickListener() {
+                deadMessageGroup.addListener(new ClickListener() {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
                         dataRepository.getPlatformInterface().restart();
                         super.clicked(event, x, y);
                     }
                 });
-                gameZone.addActor(deadMessageGroup);
+                ui.addActor(deadMessageGroup);
 
                 deadMessage = true;
             }
