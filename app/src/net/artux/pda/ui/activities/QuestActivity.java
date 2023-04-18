e1b95f150abe74eabc411147b2ebeb8a90c8625c
Maxim <maksim.prygunov@gmail.com>
2023-04-19 00:37:12 +0400

    fix jks location hide nav bar

--- app/src/net/artux/pda/ui/activities/QuestActivity.java
+++ app/src/net/artux/pda/ui/activities/QuestActivity.java
@@ -205,7 +205,27 @@ public class QuestActivity extends FragmentActivity implements AndroidFragmentAp
         switcher.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
         switcher.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
 
+        hideNavBar();
+
         startLoading();
+    }
+
+    void hideNavBar() {
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+                    if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                        decorView.setSystemUiVisibility(flags);
+                    }
+                });
     }
 
     private void startLoading() {
