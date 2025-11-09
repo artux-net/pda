81d58698a220b8ad2aa44a5dd781c86b9d1d970b
prygunovx <maksim.prygunov@gmail.com>
2025-11-09 15:03:02 +0400

    Refactor build configuration and update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,20 @@
-
-
-
-buildscript{
-    repositories{
+buildscript {
+    repositories {
         google()
         mavenLocal()
         mavenCentral()
 
-        maven( "https://jitpack.io" )
+        maven("https://jitpack.io")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://oss.sonatype.org/content/repositories/releases/")
         maven("https://artifactory.appodeal.com/appodeal")
     }
 
-    dependencies{
-        val KOTLIN = "2.0.0"
-        val DAGGER = "2.51.1"
-
+    dependencies {
         classpath("com.squareup:javapoet:1.13.0")
         classpath("com.android.tools.build:gradle:8.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${KOTLIN}")
-
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${KOTLIN}")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:${DAGGER}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
 
         classpath("com.google.gms:google-services:4.4.2")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
@@ -32,5 +24,5 @@ buildscript{
 include(
     ":app",
     "core",
-    ":model",
+    ":model"
 )
